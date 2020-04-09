acabeb4586844f4205054ce210b8b3c0f766d068
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-08 23:16:14 -0500

    add images file

--- pseudocode.js
+++ pseudocode.js
@@ -1,4 +1,4 @@
-/* game icons come from https://icons8.com/license
+/* game icons of dice1, dice2, dice3, dice4, dice5 and dice6 come from https://icons8.com/license
 
 Inspirations came from the following sites and videos:
 
