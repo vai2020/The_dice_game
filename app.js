e9228a0f8cb40839401a7bf80ea3e0a4ca03908b
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-11 02:46:02 -0500

    fix app.js to include +1 to the Math.floor(Math.random) function to exclude 0 from the random numbers

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 /*In order to roll the dice, we set up Math.random function which generates random numbers from 0 to 1 (including 0 but not including 1).  Since the results are in decimals, we add Math.floor to make it a whole number
  */
 
-let randomDice = Math.floor(Math.random() * 6);
+let randomDice = Math.floor(Math.random() * 6) + 1;
     
+console.log(randomDice);
