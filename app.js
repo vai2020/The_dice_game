1822036ed7c9f79ca3b3a81163c766e478639935
Student_M5 <student_m5@PS-DAL-M5.lan>
2020-04-11 12:08:31 -0500

    plugin randomDice function in html to roll the two dices

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 /*In order to roll the dice, we set up Math.random function which generates random numbers from 0 to 1 (including 0 but not including 1).  Since the results are in decimals, we add Math.floor and +1 to make it a whole number
  */
-
-// let randomDice = Math.floor(Math.random() * 6) + 1;
     
-//Experiment with a function randomDice to store the randomly generated number rather than in a variable
- function randomDice() {
-     return Math.floor(Math.random() * 6) + 1;
-    }
-console.log(randomDice());
+//Experiment with a function randomDice below to store the randomly generated number rather than in a variable
+//  function randomDice() {
+//     return Math.floor(Math.random() * 6) + 1;
+// }
+// console.log(randomDice());
+
+//Experient with jQuery below to store the random numbers
+
+// $(document).ready(function() {
+//     $("img").click(function() {
+//         let randomDice = Math.floor(Math.random() * 6) + 1;
+//     });
+    
+// });
+
+// console.log(randomDice);
     
